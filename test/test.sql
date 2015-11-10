56baa0e97768b296abb2da2c9296d85a44042081
Chibaheit <chibakizuna@gmail.com>
2015-11-10 16:06:59 +0800

    remove test/test.sql

--- test/test.sql
+++
@@ -1,4 +0,0 @@
-create table student (sno char(3), sname char(3) unique, sage int, sgender char(1), primary key(sno));
-create table student (sno char(3), sname char(3) unique, sage int, sgender char(1), primary key(sno));
-insert into student values ('dsf', 'adf', 1, 'f');
-select * from student where sno = 'dsf';
